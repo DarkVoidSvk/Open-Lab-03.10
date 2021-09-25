ca973c72d94764244ba87d4058daad9759deb9d5
DarkVoidSvk <samueljakubecsk@gmail.com>
2021-09-25 10:11:03 +0200

    Implement method that returns how many times a letter appears in the string.

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Linq; //The reason I added this library is because I want to use string.Count
 
 namespace Open_Lab_03._10
 {
     public class Checker
     {
-        public int GetNumberOfCharsInString(char letter, string str)
+        public int GetNumberOfCharsInString(char letter, string words)
         {
-            throw new NotImplementedException();
+            return words.Count(str => (str == letter));
         }
     }
 }
